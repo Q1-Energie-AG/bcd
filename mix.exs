2d6cf5a48386f87312e3756f802e9b566536f57a
kevin <mail@kseidel.org>
2021-10-01 12:10:47 +0200

    Downgrade minmal elixir version

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Bcd.MixProject do
     [
       app: :bcd,
       version: @version,
-      elixir: "~> 1.12",
+      elixir: "~> 1.9",
       description: "A BCD encoder for Elixir",
       package: package(),
       deps: deps(),
